d01f9371934a5c143ab858fa4067d23dbb974082
kavishkamk <kavishkamadhushan727@gmail.com>
2023-07-15 14:00:01 +0530

    feature: add entity framework and Category entity

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        [Required]
+        public required string Name { get; set; }
+        public int DisplayOrder { get; set; }
+
+    }
+}
