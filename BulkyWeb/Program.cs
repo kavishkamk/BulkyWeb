44c77c900b9b0f491f9b643bf0cea326277b19f2
kavishkamk <madhushan@dxdy.tech>
2025-03-31 01:11:32 +0530

    test project

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Test comment
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
