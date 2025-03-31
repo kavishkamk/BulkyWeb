4639968a720991eff767d60027345d0efce0e42d
kavishkamk <madhushan@dxdy.tech>
2025-03-31 20:44:53 +0530

    test

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // test 3
 // test 4
 // test 5
+// test 6
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
         options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
